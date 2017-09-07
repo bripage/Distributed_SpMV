0dd9c641eacca075a596dd4b84968b9c5c861ce0
bripage <bpage1@nd.edu>
2017-09-07 17:15:29 -0400

    .

--- main.cpp
+++ main.cpp
@@ -151,7 +151,7 @@ int main(int argc, char *argv[]) {
         nodeCSR = new csrSpMV;
     }
 
-    std::cout << "MADE IT!" << std::endl
+    std::cout << "MADE IT!" << std::endl;
 
     if (control.masterOnly != true) {
         // master to send data to cluster column masters
