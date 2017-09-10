1e576e926f1897168746e99b79651905494328cd
Brian Page <bap440@humboldt.edu>
2017-09-10 16:50:45 -0400

    I have compiled all results and performed the data conversion to *.csv. I have also made several surface charts that will be used in the paper.

---
+++ results/nd24k_surfacePlot.py
@@ -0,0 +1,47 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+import pandas as pd
+
+# Read data from a csv
+z_data = pd.read_csv('nd24k_surface.csv')
+
+data = [
+    go.Surface(
+        z=z_data.as_matrix()
+    )
+]
+layout = go.Layout(
+    title='ND24K',
+    autosize=False,
+    width=750,
+    height=750,
+    margin=dict(
+        l=65,
+        r=50,
+        b=65,
+        t=90
+    ),
+    xaxis=dict(
+        title='x-axis',
+        titlefont=dict(
+            family='Arial, sans-serif',
+            size=18,
+            color='lightgrey'
+        ),
+    ),
+    yaxis=dict(
+        autorange='reversed',
+        title='y-axis',
+        titlefont=dict(
+            family='Arial, sans-serif',
+            size=18,
+            color='lightgrey'
+        ),
+    ),
+
+)
+fig = go.Figure(data=data, layout=layout)
+py.plot(fig, filename='ND24K_SurfaceChart')
+
+
