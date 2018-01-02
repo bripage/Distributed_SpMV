7ef7ae2406f2b98cefb89a0945f609b9fd6ecd9c
Brian Page <bap440@humboldt.edu>
2018-01-01 20:07:35 -0500

    trying somethign different

--- csrSpMV.cpp
+++ csrSpMV.cpp
@@ -222,13 +222,13 @@ void csrSpMV::masterOnlySpMV(controlData control) {
 		if (i != control.rowCount - 1) {
 			for (int j = csrRows[i]; j < csrRows[i + 1]; j++) {   // go to end of current row
 				// entire row is multiplied by a single dense vector element
-				std::cout << i << "," << j << " - " << csrData[j] << " * " << denseVec[csrCols[j]] << std::endl;
+				//std::cout << i << "," << j << " - " << csrData[j] << " * " << denseVec[csrCols[j]] << std::endl;
 				temp += csrData[j] * (double) denseVec[csrCols[j]];
 			}
 		} else {
 			for (int j = csrRows[i]; j < csrData.size(); j++) {  // go to end of data vector
 				// entire row is multiplied by a single dense vector element
-				std::cout << i << "," << j << " - " << csrData[j] << " * " << denseVec[csrCols[j]] << std::endl;
+				//std::cout << i << "," << j << " - " << csrData[j] << " * " << denseVec[csrCols[j]] << std::endl;
 				temp += csrData[j] * (double) denseVec[csrCols[j]];
 			}
 		}
