c02ee99dae3a55b3ca2d44e6f18b687fe20b8348
Brian Page <bap440@humboldt.edu>
2018-01-02 06:30:08 -0500

    trying somethign different

--- main.cpp
+++ main.cpp
@@ -320,7 +320,7 @@ int main(int argc, char *argv[]) {
 
     std::vector<double> result;
 	result.resize(control.rowsPerNode, 0.0);
-    nodeCSR->result.resize(control.rowsPerNode, 0.0);
+
 
 	usleep(100000*control.myId);
 	std::cout << std::endl;
@@ -377,12 +377,12 @@ int main(int argc, char *argv[]) {
 			        if (i == nodeCSR->csrRows.size() - 1) {
 				        for (j = nodeCSR->csrRows[i]; j < nodeCSR->csrData.size(); j++) {
 					        std::cout << i << ", " << j << ", " << nodeCSR->csrCols[j] << std::endl;
-					        nodeCSR->result[i] += nodeCSR->csrData[j] * (double) nodeCSR->denseVec[nodeCSR->csrCols[j]];
+					        result[i] += nodeCSR->csrData[j] * (double) nodeCSR->denseVec[nodeCSR->csrCols[j]];
 				        }
 			        } else {
 				        for (j = nodeCSR->csrRows[i]; j < nodeCSR->csrRows[i + 1]; j++) {
 					        std::cout << i << ", " << j << ", " << nodeCSR->csrCols[j] << ", " << nodeCSR->csrData[j] << " * " << nodeCSR->denseVec[nodeCSR->csrCols[j]] << std::endl;
-					        nodeCSR->result[i] += nodeCSR->csrData[j] * (double) nodeCSR->denseVec[nodeCSR->csrCols[j]];
+					        result[i] += nodeCSR->csrData[j] * (double) nodeCSR->denseVec[nodeCSR->csrCols[j]];
 				        }
 			        }
 		        }
@@ -392,12 +392,12 @@ int main(int argc, char *argv[]) {
 			        if (i == nodeCSR->csrRows.size() - 1) {
 				        for (j = nodeCSR->csrRows[i]; j < nodeCSR->csrData.size(); j++) {
 					        std::cout << i << ", " << j << ", " << nodeCSR->csrCols[j] << std::endl;
-					        nodeCSR->result[i] += nodeCSR->csrData[j] * (double) nodeCSR->denseVec[nodeCSR->csrCols[j]];
+					        result[i] += nodeCSR->csrData[j] * (double) nodeCSR->denseVec[nodeCSR->csrCols[j]];
 				        }
 			        } else {
 				        for (j = nodeCSR->csrRows[i]; j < nodeCSR->csrRows[i + 1]; j++) {
 					        std::cout << i << ", " << j << ", " << nodeCSR->csrCols[j] << std::endl;
-					        nodeCSR->result[i] += nodeCSR->csrData[j] * (double) nodeCSR->denseVec[nodeCSR->csrCols[j]];
+					        result[i] += nodeCSR->csrData[j] * (double) nodeCSR->denseVec[nodeCSR->csrCols[j]];
 				        }
 			        }
 		        }
