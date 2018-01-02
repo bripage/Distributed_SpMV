36d3e9e21fc86d6a032821d8c0b270e2537911fd
Brian Page <bap440@humboldt.edu>
2018-01-01 19:55:24 -0500

    trying somethign different

--- main.cpp
+++ main.cpp
@@ -302,6 +302,7 @@ int main(int argc, char *argv[]) {
             MPI_Recv(&nodeCSR->csrData[0], control.elementCount, MPI_DOUBLE, 0, 0, control.col_comm, MPI_STATUS_IGNORE);
             //MPI_Recv(&nodeCSR->denseVec[0], control.rowCount, MPI_DOUBLE, 0, 0, control.col_comm, MPI_STATUS_IGNORE);
 
+	        std::cout << "nodeCSR->rebase(" << control.myCol << " * " << control.colsPerNode << ")" << std::endl;
 	        nodeCSR->rebase(control.myCol * control.colsPerNode);
         }
 
