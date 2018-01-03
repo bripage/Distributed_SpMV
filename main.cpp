6004bce3b72b6138ca18b79f3414ce96e14f72f0
Brian Page <bap440@humboldt.edu>
2018-01-02 20:20:53 -0500

    trying somethign different

--- main.cpp
+++ main.cpp
@@ -376,7 +376,7 @@ int main(int argc, char *argv[]) {
 			        //std::cout << "ompThreadId =  " << ompThreadId << ", " << i << std::endl;
 			        if (i == rowEnd - 1) {
 				        for (j = nodeCSR->csrRows[i]; j < nodeCSR->csrData.size(); j++) {
-					        if (control.myId == 1) std::cout << ompThreadId << ", " << i << ", " << j << ", " << nodeCSR->csrCols[j] << std::endl;
+					        //if (control.myId == 1) std::cout << ompThreadId << ", " << i << ", " << j << ", " << nodeCSR->csrCols[j] << std::endl;
 					        //if (control.myId == 0)std::cout << nodeCSR->csrCols[j] << ", " << nodeCSR->csrData[j] << " * " << nodeCSR->denseVec[nodeCSR->csrCols[j]] << std::endl;
 					        result[i] += nodeCSR->csrData[j] * (double) nodeCSR->denseVec[nodeCSR->csrCols[j]];
 				        }
@@ -391,19 +391,11 @@ int main(int argc, char *argv[]) {
 		        }
 	        } else {
 		        for (i = ompThreadId * rowsPerThread; i < rowEnd; i++) {
-			        //if (control.myId == 0) std::cout << "ompThreadId =  " << ompThreadId << ", " << i << std::endl;
-			        if (i == rowEnd - 1) {
-				        for (j = nodeCSR->csrRows[i]; j < nodeCSR->csrData.size(); j++) {
-					        //if (control.myId == 1) std::cout << ompThreadId << ", " <<i << ", " << j << ", " << nodeCSR->csrCols[j] << std::endl;
-					        result[i] += nodeCSR->csrData[j] * (double) nodeCSR->denseVec[nodeCSR->csrCols[j]];
-				        }
-			        } else {
-				        for (j = nodeCSR->csrRows[i]; j < nodeCSR->csrRows[i + 1]; j++) {
-					        //if (control.myId == 1) std::cout << ompThreadId << ", " <<i << ", " << j << ", " << nodeCSR->csrCols[j] << std::endl;
-					        result[i] += nodeCSR->csrData[j] * (double) nodeCSR->denseVec[nodeCSR->csrCols[j]];
-				        }
+			        for (j = nodeCSR->csrRows[i]; j < nodeCSR->csrRows[i + 1]; j++) {
+				        if (control.myId == 1) std::cout << ompThreadId << ", " <<i << ", " << j << ", " << nodeCSR->csrCols[j] << std::endl;
+				        result[i] += nodeCSR->csrData[j] * (double) nodeCSR->denseVec[nodeCSR->csrCols[j]];
 			        }
-		        }
+			     }
 	        }
         }
     }
