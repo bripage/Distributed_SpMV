e332d24de3f4e65092b74d15cb23b14d63b0ba45
Brian Page <bap440@humboldt.edu>
2018-01-28 00:04:15 -0500

    added processor callout debugging to verify affinity settings

--- main.cpp
+++ main.cpp
@@ -94,6 +94,15 @@ int main(int argc, char *argv[]) {
         }
     }
 
+	// Get the name of the processor
+	if (contro.debug) {
+		usleep(100000 * control.myId);
+		char processor_name[MPI_MAX_PROCESSOR_NAME];
+		int name_len;
+		MPI_Get_processor_name(processor_name, &name_len);
+		std::cout << "Process " << control.myId << "is on process" << processor_name << std::endl;
+	}
+
     //****************************************************//
     //  Create comm for each column/row of compute nodes  //
     //****************************************************//
