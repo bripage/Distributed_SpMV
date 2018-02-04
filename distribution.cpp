3774d40f37075acc0da43d32f16714fd1c3b2140
Brian Page <bap440@humboldt.edu>
2018-02-04 06:07:38 -0500

    trying to implemenet balanced NNZ per process distribution

--- distribution.cpp
+++ distribution.cpp
@@ -327,9 +327,9 @@ void distribution_Balanced(controlData& control, std::vector<csrSpMV*>& clusterC
 	for (int i = 0; i < control.processCount; i++){
 		for (int j = 0; j < distributionRows.size(); j++){
 			if (distributionRows[i].processAssignment == i){
-				std::cout << " istributionRows[" << i << "].processAssignment = " << distributionRows[i].processAssignment << std::endl;
+				//std::cout << " distributionRows[" << i << "].processAssignment = " << distributionRows[i].processAssignment << std::endl;
 				clusterColData[i%control.clusterCols]->processData[((i/control.clusterRows)*3)+1]+=1;
-				std::cout << "clusterColData[" << i%control.clusterCols << "]->processData["
+				//std::cout << "clusterColData[" << i%control.clusterCols << "]->processData["
 				          << ((i/control.clusterRows)*3)+1 << "] = "
 				          << clusterColData[i%control.clusterCols]->processData[((i/control.clusterRows)*3)+1]
 				          << std::endl;
