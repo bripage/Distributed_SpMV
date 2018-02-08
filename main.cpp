6b08a9b293bdb7f6fe3233aa8d0bbf03c2dd9903
Brian Page <bap440@humboldt.edu>
2018-02-08 04:21:07 -0500

    trying to implemenet balanced NNZ per process distribution

--- main.cpp
+++ main.cpp
@@ -953,7 +953,7 @@ int main(int argc, char *argv[]) {
 					MPI_Send(&control.rowCount, 1, MPI_INT, i, 0, control.col_comm);
 					//std::cout << "sending " << nodeCSR->processData.size() << " processData elements to " << i << std::endl;
 					MPI_Send(&(nodeCSR->processData[(i*2)]), 2, MPI_INT, i, 0, control.col_comm);
-					std::cout <<  "sent " << nodeCSR->processData[i*2] << ", " << nodeCSR->processData[(i*2)+1] << std::endl;
+					//std::cout <<  "sent " << nodeCSR->processData[i*2] << ", " << nodeCSR->processData[(i*2)+1] << std::endl;
 
 					MPI_Send(&(nodeCSR->assignedRowIds[rowsSent]), nodeCSR->processData[(i*2)+1], MPI_INT, i, 0,
 					         control.col_comm);
